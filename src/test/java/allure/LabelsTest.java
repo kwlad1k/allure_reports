e95c0a848e19b88457befe765a45d7ce1541eaed
vladislavkadyrov <vlad-kad00@mail.ru>
2023-02-12 00:43:38 +0500

    initial commit

---
+++ src/test/java/allure/LabelsTest.java
@@ -0,0 +1,13 @@
+package allure;
+
+import org.junit.jupiter.api.Test;
+
+public class LabelsTest {
+    @Test
+    public void testStaticLabels() {
+    }
+
+    @Test
+    public void testDynamicLabels () {
+    }
+}
---
+++ src/test/java/allure/StepTest.java
@@ -0,0 +1,37 @@
+package allure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.attachment;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+
+public class StepTest {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 81;
+
+    @Test
+    public void testLamdaStep  () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        step("Открываем главную страницу", () -> {
+            open("https://github.com");
+            attachment("Source", webdriver().driver().source());
+        });
+    }
+
+    @Test
+    public void testAnnotatedStep () {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+
+
+    }
+}
