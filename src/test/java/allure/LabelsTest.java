755f90daa4e7ad01aa6fedff4194e4fda678714e
vladislavkadyrov <vlad-kad00@mail.ru>
2023-02-12 12:31:57 +0500

    Add Labels

--- src/test/java/allure/LabelsTest.java
+++ src/test/java/allure/LabelsTest.java
@@ -1,13 +1,21 @@
 package allure;
 
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LabelsTest {
     @Test
+    @Feature("Issue в репозитории")
+    @Story("Сознадние Issue")
+    @Owner("Kwlad1ck")
+    @Severity(SeverityLevel.BLOCKER )
+    @Link(value = "GitHub", url = "https://github.com/kwlad1k")
+    @DisplayName("Создание Issue для авторизованного пользователя")
     public void testStaticLabels() {
     }
 
     @Test
-    public void testDynamicLabels () {
+    public void testDynamicLabels() {
     }
 }
